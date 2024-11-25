6191beeffba173ef114763132589c3796226aa1b
IT21068850 <it21068850@my.sliit.lk>
2024-11-25 15:14:15 +0530

    Started implementing Websrcapping.py

--- WebScraping.py
+++ WebScraping.py
@@ -1,6 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-baseurl  = 'https://ikman.lk/en/ads/sri-lanka/cars'
+baseurl  = 'https://ikman.lk'
+
+headers = {
+
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
+}
 
 
+r = requests.get('https://ikman.lk/en/ads/sri-lanka/cars')
+soup = BeautifulSoup(r.content,'lxml')
+
+productList = soup.findAll('li', class_ = 'normal--2QYVk gtm-normal-ad')
+
+#print(productList)
+
+productLinks =[]
+
+for items in productList:
+    for link in items.findAll('a', href = True):
+        # print(link['href'])
+        productLinks.append(baseurl + link['href'])
+
+
+
+print(len(productLinks))
